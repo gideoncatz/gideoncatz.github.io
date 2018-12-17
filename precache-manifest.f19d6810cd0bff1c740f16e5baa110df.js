9a7fdaa476ff168b2896354347b0a88873f710d1
MATRIX\gideonca <gideonca@matrix.co.il>
2018-12-17 16:34:56 +0200

    initial commit

---
+++ precache-manifest.f19d6810cd0bff1c740f16e5baa110df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6b6e2b7b966120f32030",
+    "url": "/js/chunk-vendors.851407d7.js"
+  },
+  {
+    "revision": "c52c9e43ece273a5e39d",
+    "url": "/js/app.5b520677.js"
+  },
+  {
+    "revision": "8871b0dd4d9fc9649819",
+    "url": "/js/about.a956700f.js"
+  },
+  {
+    "revision": "8a0f7a1c4f35fe98f2c0c71e5c26172d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c52c9e43ece273a5e39d",
+    "url": "/css/app.a0398b14.css"
+  }
+];
