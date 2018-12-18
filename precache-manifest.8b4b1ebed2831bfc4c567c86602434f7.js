1eb1042dedfc6b66715ebf7a4a060a84bda814f4
MATRIX\gideonca <gideonca@matrix.co.il>
2018-12-18 13:01:00 +0200

    jumpstart with popup

---
+++ precache-manifest.8b4b1ebed2831bfc4c567c86602434f7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5f8db399ff7d20c8a69",
+    "url": "/js/contact.ab2c9293.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "62a8097c45fd54cb2629",
+    "url": "/js/student.4fb4af88.js"
+  },
+  {
+    "revision": "8c4b286ba98566393251",
+    "url": "/css/about.f1372490.css"
+  },
+  {
+    "revision": "9443c5513c09ae679f9c",
+    "url": "/css/app.6e940290.css"
+  },
+  {
+    "revision": "9443c5513c09ae679f9c",
+    "url": "/js/app.a987721a.js"
+  },
+  {
+    "revision": "aafb35aa24b3838285e6",
+    "url": "/js/chunk-vendors.8e3bfb47.js"
+  },
+  {
+    "revision": "e5f8db399ff7d20c8a69",
+    "url": "/css/contact.95ca2c27.css"
+  },
+  {
+    "revision": "c71d0efc603e4b9fb297",
+    "url": "/js/ExcelUpload.636be68e.js"
+  },
+  {
+    "revision": "8c4b286ba98566393251",
+    "url": "/js/about.11023a3a.js"
+  },
+  {
+    "revision": "7a0f78f66a903bcb5c42",
+    "url": "/js/teacher.c9d2e7b7.js"
+  },
+  {
+    "revision": "57b188f068d4bb6f10a3",
+    "url": "/css/tos.86267e2e.css"
+  },
+  {
+    "revision": "57b188f068d4bb6f10a3",
+    "url": "/js/tos.baf08466.js"
+  },
+  {
+    "revision": "b72f9006ba795a5dc62484f8d263592b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb0aea378d5d729f0a862be08b728a88",
+    "url": "/login-popup-callback.html"
+  },
+  {
+    "revision": "c71d0efc603e4b9fb297",
+    "url": "/css/ExcelUpload.7f2ed77d.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05a4fd250b4a42888fa88bfe04dfe4a4.js"
+  "/precache-manifest.8b4b1ebed2831bfc4c567c86602434f7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "jumpstart-frontend"});
